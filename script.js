cb1ae36bc5c885a66b5d6cd077101880e1c169b9
jonathanfrei3 <82921669+jonathanfrei3@users.noreply.github.com>
2026-02-09 01:47:08 -0400

    script do portfólio

---
+++ script.js
@@ -0,0 +1,18 @@
+// efeito de entrada suave
+const elements = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+});
+
+elements.forEach(el => {
+  el.style.opacity = 0;
+  el.style.transform = "translateY(40px)";
+  el.style.transition = "1s";
+  observer.observe(el);
+});
